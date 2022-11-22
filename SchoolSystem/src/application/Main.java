8d71fd375823ffa27767f1d72afb4e92574b8a6a
raz228822 <raz228822@gmail.com>
2022-11-22 18:31:18 +0200

    dsds

--- SchoolSystem/src/application/Main.java
+++ SchoolSystem/src/application/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) { //first commit
+	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
