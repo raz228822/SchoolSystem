9b27ce1a41a9c8546a172364c4f38030cf12512c
raz228822 <raz228822@gmail.com>
2022-11-22 18:42:16 +0200

    first commit

--- SchoolSystem/src/application/Main.java
+++ SchoolSystem/src/application/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) { //RAZ and DOR
+	public void start(Stage primaryStage) { //first commit
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
